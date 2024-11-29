57d8eca6a15a925ea0af52a2820e5817b68db8cb
mono <monoberutu58@gmail.com>
2024-11-29 12:56:18 +0700

    perubahan profil

--- controller/user_profil_controller.go
+++ controller/user_profil_controller.go
@@ -3,6 +3,7 @@ package controller
 import (
 	"calmind/helper"
 	"calmind/model"
+	"calmind/service"
 	"calmind/usecase"
 	"net/http"
 
@@ -16,14 +17,52 @@ type ProfilController struct {
 func NewProfilController(ProfilUsecase usecase.UserProfilUsecase) *ProfilController {
 	return &ProfilController{ProfilUsecase: ProfilUsecase}
 }
-func (c *ProfilController) GetProfile(ctx echo.Context) error {
-	userClaims := ctx.Get("user").(*model.User)
 
-	// Mendapatkan data profil user berdasarkan ID
-	user, err := c.ProfilUsecase.GetUserProfile(userClaims.ID)
+func (c *ProfilController) GetProfile(ctx echo.Context) error {
+	claims, _ := ctx.Get("user").(*service.JwtCustomClaims)
+
+	user, err := c.ProfilUsecase.GetUserProfile(claims.UserID)
 	if err != nil {
 		return helper.JSONErrorResponse(ctx, http.StatusInternalServerError, "Gagal mengambil profil: "+err.Error())
 	}
 
-	return helper.JSONSuccessResponse(ctx, user)
+	type UserProfileResponse struct {
+		ID       int    `json:"id"`
+		Username string `json:"username"`
+		NoHp     string `json:"no_hp"`
+		Email    string `json:"email"`
+		Avatar   string `json:"avatar"`
+		FullName string `json:"full_name"`
+		Bio      string `json:"bio"`
+	}
+
+	userProfile := UserProfileResponse{
+		ID:       user.ID,
+		Username: user.Username,
+		NoHp:     user.NoHp,
+		Email:    user.Email,
+		Avatar:   user.Avatar,
+		Bio:      user.Bio,
+	}
+
+	return helper.JSONSuccessResponse(ctx, userProfile)
+}
+
+func (c *ProfilController) UpdateProfile(ctx echo.Context) error {
+	claims, _ := ctx.Get("user").(*service.JwtCustomClaims)
+	var user model.User
+
+	if err := ctx.Bind(&user); err != nil {
+		return helper.JSONErrorResponse(ctx, http.StatusBadRequest, "Gagal mendapatkan data: "+err.Error())
+	}
+
+	user.Email = ""
+	user.Password = ""
+
+	updatedUser, err := c.ProfilUsecase.UpdateUserProfile(claims.UserID, &user)
+	if err != nil {
+		return helper.JSONErrorResponse(ctx, http.StatusInternalServerError, "Gagal mengupdate profil: "+err.Error())
+	}
+
+	return helper.JSONSuccessResponse(ctx, updatedUser)
 }
