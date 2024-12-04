61c0b0e91a634fce332528021d14b234ecff960b
mono <monoberutu58@gmail.com>
2024-12-04 18:38:18 +0700

    cors setting

--- model/admin.go
+++ model/admin.go
@@ -1,10 +1,10 @@
 package model
 
 type Admin struct {
-	ID       int       `gorm:"primaryKey;autoIncrement" json:"id"`
+	ID       int       `gorm:"primaryKey" json:"id"`
 	Username string    `gorm:"not null" json:"username"`
 	Email    string    `gorm:"unique;not null" json:"email"`
-	Password string    `gorm:"not null" json:"-"`
+	Password string    `gorm:"not null" json:"password"`
 	Role     string    `gorm:"not null" json:"role"`
-	Artikels []Artikel `gorm:"foreignKey:AdminID" json:"artikels,omitempty"` // Relasi ke Artikel
+	Artikels []Artikel `gorm:"foreignKey:AdminID" json:"artikels"` // Relasi ke Artikel
 }
--- model/artikel.go
+++ model/artikel.go
@@ -3,9 +3,9 @@ package model
 import "time"
 
 type Artikel struct {
-	ID        int       `gorm:"primaryKey;autoIncrement" json:"id"`
-	AdminID   int       `gorm:"not null" json:"admin_id"`                       // Foreign key
-	Admin     Admin     `gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"` // Relasi ke Admin
+	ID        int       `gorm:"primaryKey" json:"id"`
+	AdminID   int       `gorm:"not null" json:"admin_id"` // Foreign key
+	Admin     Admin     `json:"admin"`                    // Relasi ke Admin
 	Judul     string    `gorm:"not null" json:"judul"`
 	Gambar    string    `json:"gambar"`
 	Isi       string    `gorm:"not null" json:"isi"`
