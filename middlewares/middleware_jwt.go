e6c0c564eddb13ffd5d32c3a6246f3ab38c074ab
mono <monoberutu58@gmail.com>
2024-12-04 19:12:43 +0700

    cors setting

--- middlewares/middleware_jwt.go
+++ middlewares/middleware_jwt.go
@@ -20,12 +20,12 @@ func NewJWTMiddleware(cfg *config.JWTConfig) *JWTMiddleware {
 
 func (m *JWTMiddleware) HandlerAdmin(next echo.HandlerFunc) echo.HandlerFunc {
 	return func(c echo.Context) error {
-		h, err := c.Cookie("token_admin")
+		cookie, err := c.Cookie("token_admin")
 		if err != nil {
 			return helper.JSONErrorResponse(c, http.StatusUnauthorized, "Token admin tidak ditemukan")
 		}
 
-		tokenString := h.Value
+		tokenString := cookie.Value
 		claims := &service.JwtCustomClaims{}
 
 		token, err := jwt.ParseWithClaims(tokenString, claims, func(t *jwt.Token) (interface{}, error) {
@@ -40,13 +40,12 @@ func (m *JWTMiddleware) HandlerAdmin(next echo.HandlerFunc) echo.HandlerFunc {
 			return helper.JSONErrorResponse(c, http.StatusForbidden, "Akses hanya untuk admin")
 		}
 
-		// Validasi bahwa admin_id tidak kosong
-		if claims.UserID == 0 {
+		if claims.UserID <= 0 {
 			return helper.JSONErrorResponse(c, http.StatusUnauthorized, "Klaim JWT tidak valid atau admin_id kosong")
 		}
 
-		// Set klaim ke context
-		c.Set("admin", claims)
+		c.Set("admin_id", claims.UserID)
+
 		return next(c)
 	}
 }
--- model/artikel.go
+++ model/artikel.go
@@ -3,10 +3,10 @@ package model
 import "time"
 
 type Artikel struct {
-	ID        int       `gorm:"primaryKey;autoIncrement" json:"id"`                          // Primary key
-	AdminID   int       `gorm:"not null;index" json:"admin_id"`                              // Foreign key dengan index
-	Admin     Admin     `gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL;" json:"admin"` // Relasi ke Admin
-	Judul     string    `gorm:"type:varchar(255);not null" json:"judul"`                     // Panjang maksimum judul ditentukan
+	ID        int       `gorm:"primaryKey;autoIncrement" json:"id"`
+	AdminID   int       `gorm:"not null" json:"admin_id"`
+	Admin     Admin     `gorm:"foreignKey:AdminID" json:"admin"`
+	Judul     string    `gorm:"not null" json:"judul"`
 	Gambar    string    `json:"gambar"`
 	Isi       string    `gorm:"not null" json:"isi"`
 	CreatedAt time.Time `gorm:"autoCreateTime" json:"created_at"`
