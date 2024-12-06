7d7c075d5a0e3ea67c0fc7eaef433e08ee8dee02
mono <monoberutu58@gmail.com>
2024-12-06 12:48:27 +0700

    perbaikan

--- main.go
+++ main.go
@@ -83,21 +83,10 @@ func main() {
 	// Echo instance
 	e := echo.New()
 	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
-		AllowOrigins: []string{
-			"http://localhost:3000",                      // Tambahkan domain frontend lokal
-			"http://localhost:5173",                      // Tambahkan domain frontend lokal
-			"https://jovial-mooncake-23a3d0.netlify.app", // Domain frontend produksi
-		},
-		AllowMethods: []string{
-			echo.GET, echo.POST, echo.PUT, echo.DELETE, echo.OPTIONS, // Pastikan OPTIONS diizinkan
-		},
-		AllowHeaders: []string{
-			echo.HeaderAuthorization, echo.HeaderContentType, echo.HeaderAccept, "X-Requested-With",
-		},
-		ExposeHeaders: []string{
-			"Authorization",
-		},
-		AllowCredentials: false, // Tidak perlu jika hanya menggunakan token di header Authorization
+		AllowOrigins:     []string{"http://127.0.0.1:5500", "http://localhost:3000", "http://localhost:5173", "https://jovial-mooncake-23a3d0.netlify.app"}, // Sesuaikan dengan domain frontend Anda
+		AllowMethods:     []string{echo.GET, echo.POST, echo.PUT, echo.DELETE},
+		AllowHeaders:     []string{echo.HeaderAuthorization, echo.HeaderContentType},
+		AllowCredentials: false, // Izinkan pengiriman cookie
 	}))
 
 	// routes auth
