38f07ff5892fe73e8988a7523b4f3c54bac25386
mono <monoberutu58@gmail.com>
2024-12-09 18:04:12 +0700

    perbaikan

--- usecase/konsultasi_usecase.go
+++ usecase/konsultasi_usecase.go
@@ -6,26 +6,24 @@ import (
 	"errors"
 	"fmt"
 	"os"
-	"time"
 
 	"github.com/midtrans/midtrans-go"
-	"github.com/midtrans/midtrans-go/coreapi"
 	"github.com/midtrans/midtrans-go/snap"
 )
 
 type ConsultationUsecase interface {
 	CreateConsultation(userID, doctorID int, title, description, email string) (string, error)
-	GetConsultationByID(consultationID int) (*model.Consultation, error)
-	MarkExpiredConsultations() error
-	ApprovePayment(adminID, consultationID int) error
-	ViewConsultationDetailsForAdmin(consultationID int) (*model.Consultation, error)
-	GetConsultationsForDoctor(doctorID int) ([]model.Consultation, error)
-	ViewConsultationDetails(doctorID, consultationID int) (*model.Consultation, error)
-	AddRecommendation(doctorID, consultationID int, recommendation string) error
-	GetUserConsultations(userID int) ([]model.Consultation, error)
-	GetPendingConsultationsForAdmin() ([]model.Consultation, error)
-	CreateMidtransPayment(consultationID int, amount float64, email string) (string, error)
-	VerifyPayment(consultationID int) (string, error)
+	// GetConsultationByID(consultationID int) (*model.Consultation, error)
+	// MarkExpiredConsultations() error
+	// ApprovePayment(adminID, consultationID int) error
+	// ViewConsultationDetailsForAdmin(consultationID int) (*model.Consultation, error)
+	// GetConsultationsForDoctor(doctorID int) ([]model.Consultation, error)
+	// ViewConsultationDetails(doctorID, consultationID int) (*model.Consultation, error)
+	// AddRecommendation(doctorID, consultationID int, recommendation string) error
+	// GetUserConsultations(userID int) ([]model.Consultation, error)
+	// GetPendingConsultationsForAdmin() ([]model.Consultation, error)
+	// CreateMidtransPayment(consultationID int, amount float64, email string) (string, error)
+	// VerifyPayment(consultationID int) (string, error)
 }
 
 type ConsultationUsecaseImpl struct {
@@ -70,142 +68,6 @@ func (uc *ConsultationUsecaseImpl) CreateConsultation(userID, doctorID int, titl
 	return paymentURL, consultation, nil
 }
 
-// Mendapatkan konsultasi berdasarkan ID
-func (uc *ConsultationUsecaseImpl) GetConsultationByID(consultationID int) (*model.Consultation, error) {
-	consultation, err := uc.Repo.GetConsultationByID(consultationID)
-	if err != nil {
-		return nil, err
-	}
-
-	// Periksa apakah konsultasi telah kedaluwarsa
-	now := time.Now()
-	endTime := consultation.StartTime.Add(time.Duration(consultation.Duration) * time.Minute)
-	if now.After(endTime) && consultation.Status != "ended" {
-		consultation.Status = "ended"
-		err = uc.Repo.UpdateConsultation(consultation)
-		if err != nil {
-			return nil, errors.New("failed to update consultation status")
-		}
-	}
-
-	return consultation, nil
-}
-
-// Menandai konsultasi yang sudah kedaluwarsa
-func (uc *ConsultationUsecaseImpl) MarkExpiredConsultations() error {
-	consultations, err := uc.Repo.GetActiveConsultations()
-	if err != nil {
-		return err
-	}
-
-	now := time.Now()
-	for _, consultation := range consultations {
-		endTime := consultation.StartTime.Add(time.Duration(consultation.Duration) * time.Minute)
-		if now.After(endTime) {
-			consultation.Status = "ended"
-			if err := uc.Repo.UpdateConsultation(&consultation); err != nil {
-				return errors.New("failed to update consultation status")
-			}
-		}
-	}
-	return nil
-}
-
-func (uc *ConsultationUsecaseImpl) ApprovePayment(consultationID int) error {
-	consultation, err := uc.Repo.GetConsultationByID(consultationID)
-	if err != nil {
-		return errors.New("consultation not found")
-	}
-
-	if consultation.IsApproved {
-		return errors.New("payment already approved")
-	}
-
-	if consultation.Doctor.Price <= 0 {
-		return errors.New("invalid doctor price")
-	}
-
-	// Tentukan pembagian biaya
-	adminFee := 0.1 * float64(consultation.Doctor.Price) // 10% biaya admin
-	doctorFee := float64(consultation.Doctor.Price) - adminFee
-
-	// Update konsultasi
-	consultation.IsApproved = true
-	consultation.Status = "active"
-	consultation.StartTime = time.Now()
-
-	err = uc.Repo.UpdateConsultation(consultation)
-	if err != nil {
-		return errors.New("failed to update consultation status")
-	}
-
-	// Tambahkan pendapatan dokter dan admin
-	err = uc.Repo.AddIncomeForDoctor(consultation.DoctorID, doctorFee)
-	if err != nil {
-		return errors.New("failed to update doctor income")
-	}
-
-	err = uc.Repo.AddIncomeForAdmin(adminFee)
-	if err != nil {
-		return errors.New("failed to update admin income")
-	}
-
-	return nil
-}
-
-// Mendapatkan detail konsultasi untuk admin
-func (uc *ConsultationUsecaseImpl) ViewConsultationDetailsForAdmin(consultationID int) (*model.Consultation, error) {
-	return uc.Repo.GetConsultationByID(consultationID)
-}
-
-// Mendapatkan daftar konsultasi untuk dokter
-func (uc *ConsultationUsecaseImpl) GetConsultationsForDoctor(doctorID int) ([]model.Consultation, error) {
-	return uc.Repo.GetConsultationsForDoctor(doctorID)
-}
-
-// Mendapatkan detail konsultasi tertentu untuk dokter
-func (uc *ConsultationUsecaseImpl) ViewConsultationDetails(doctorID, consultationID int) (*model.Consultation, error) {
-	consultation, err := uc.Repo.GetConsultationDetails(consultationID, doctorID)
-	if err != nil {
-		return nil, err
-	}
-
-	if consultation.DoctorID != doctorID {
-		return nil, errors.New("unauthorized access to consultation")
-	}
-
-	return consultation, nil
-}
-
-// Menambahkan rekomendasi untuk konsultasi
-func (uc *ConsultationUsecaseImpl) AddRecommendation(doctorID, consultationID int, recommendation string) error {
-	consultation, err := uc.Repo.GetConsultationDetails(consultationID, doctorID)
-	if err != nil {
-		return err
-	}
-
-	if consultation.DoctorID != doctorID {
-		return errors.New("unauthorized access to consultation")
-	}
-
-	recommendationObj := &model.Rekomendasi{
-		ConsultationID: consultation.ID,
-		DoctorID:       doctorID,
-		Rekomendasi:    recommendation,
-	}
-	return uc.Repo.AddRecommendation(recommendationObj)
-}
-
-// Mendapatkan daftar konsultasi dengan dokter untuk user tertentu
-func (uc *ConsultationUsecaseImpl) GetUserConsultations(userID int) ([]model.Consultation, error) {
-	return uc.Repo.GetConsultationsWithDoctors(userID)
-}
-
-// Mendapatkan daftar konsultasi yang belum disetujui pembayarannya
-func (uc *ConsultationUsecaseImpl) GetPendingConsultationsForAdmin() ([]model.Consultation, error) {
-	return uc.Repo.GetPendingConsultations()
-}
-
 func (uc *ConsultationUsecaseImpl) CreateMidtransPayment(consultationID int, amount float64, email string) (string, error) {
 	// Konfigurasi Midtrans
 	client := snap.Client{}
@@ -221,28 +83,165 @@ func (uc *ConsultationUsecaseImpl) CreateMidtransPayment(consultationID int, amo
 			Email: email,
 		},
 	}
-
-	// Generate Snap Token
-	snapTokenResp, err := client.CreateTransaction(snapReq)
-	if err != nil {
-		return "", fmt.Errorf("failed to create midtrans payment: %w", err)
-	}
-
-	return snapTokenResp.RedirectURL, nil
 }
 
-func (uc *ConsultationUsecaseImpl) VerifyPayment(consultationID int) (string, error) {
-	client := coreapi.Client{}
-	client.New(os.Getenv("MIDTRANS_SERVER_KEY"), midtrans.Sandbox) // Gunakan midtrans.Production jika live
+// // Mendapatkan konsultasi berdasarkan ID
+// func (uc *ConsultationUsecaseImpl) GetConsultationByID(consultationID int) (*model.Consultation, error) {
+// 	consultation, err := uc.Repo.GetConsultationByID(consultationID)
+// 	if err != nil {
+// 		return nil, err
+// 	}
 
-	// Gunakan orderID untuk mencari status transaksi
-	orderID := fmt.Sprintf("consultation-%d", consultationID)
+// 	// Periksa apakah konsultasi telah kedaluwarsa
+// 	now := time.Now()
+// 	endTime := consultation.StartTime.Add(time.Duration(consultation.Duration) * time.Minute)
+// 	if now.After(endTime) && consultation.Status != "ended" {
+// 		consultation.Status = "ended"
+// 		err = uc.Repo.UpdateConsultation(consultation)
+// 		if err != nil {
+// 			return nil, errors.New("failed to update consultation status")
+// 		}
+// 	}
 
-	// Panggil API untuk mendapatkan status transaksi
-	transactionStatusResp, err := client.CheckTransaction(orderID)
-	if err != nil {
-		return "", fmt.Errorf("failed to verify payment: %w", err)
-	}
+// 	return consultation, nil
+// }
 
-	return transactionStatusResp.TransactionStatus, nil
-}
+// // Menandai konsultasi yang sudah kedaluwarsa
+// func (uc *ConsultationUsecaseImpl) MarkExpiredConsultations() error {
+// 	consultations, err := uc.Repo.GetActiveConsultations()
+// 	if err != nil {
+// 		return err
+// 	}
+
+// 	now := time.Now()
+// 	for _, consultation := range consultations {
+// 		endTime := consultation.StartTime.Add(time.Duration(consultation.Duration) * time.Minute)
+// 		if now.After(endTime) {
+// 			consultation.Status = "ended"
+// 			if err := uc.Repo.UpdateConsultation(&consultation); err != nil {
+// 				return errors.New("failed to update consultation status")
+// 			}
+// 		}
+// 	}
+// 	return nil
+// }
+
+// func (uc *ConsultationUsecaseImpl) ApprovePayment(consultationID int) error {
+// 	consultation, err := uc.Repo.GetConsultationByID(consultationID)
+// 	if err != nil {
+// 		return errors.New("consultation not found")
+// 	}
+
+// 	if consultation.IsApproved {
+// 		return errors.New("payment already approved")
+// 	}
+
+// 	if consultation.Doctor.Price <= 0 {
+// 		return errors.New("invalid doctor price")
+// 	}
+
+// 	// Tentukan pembagian biaya
+// 	adminFee := 0.1 * float64(consultation.Doctor.Price) // 10% biaya admin
+// 	doctorFee := float64(consultation.Doctor.Price) - adminFee
+
+// 	// Update konsultasi
+// 	consultation.IsApproved = true
+// 	consultation.Status = "active"
+// 	consultation.StartTime = time.Now()
+
+// 	err = uc.Repo.UpdateConsultation(consultation)
+// 	if err != nil {
+// 		return errors.New("failed to update consultation status")
+// 	}
+
+// 	// Tambahkan pendapatan dokter dan admin
+// 	err = uc.Repo.AddIncomeForDoctor(consultation.DoctorID, doctorFee)
+// 	if err != nil {
+// 		return errors.New("failed to update doctor income")
+// 	}
+
+// 	err = uc.Repo.AddIncomeForAdmin(adminFee)
+// 	if err != nil {
+// 		return errors.New("failed to update admin income")
+// 	}
+
+// 	return nil
+// }
+
+// // Mendapatkan detail konsultasi untuk admin
+// func (uc *ConsultationUsecaseImpl) ViewConsultationDetailsForAdmin(consultationID int) (*model.Consultation, error) {
+// 	return uc.Repo.GetConsultationByID(consultationID)
+// }
+
+// // Mendapatkan daftar konsultasi untuk dokter
+// func (uc *ConsultationUsecaseImpl) GetConsultationsForDoctor(doctorID int) ([]model.Consultation, error) {
+// 	return uc.Repo.GetConsultationsForDoctor(doctorID)
+// }
+
+// // Mendapatkan detail konsultasi tertentu untuk dokter
+// func (uc *ConsultationUsecaseImpl) ViewConsultationDetails(doctorID, consultationID int) (*model.Consultation, error) {
+// 	consultation, err := uc.Repo.GetConsultationDetails(consultationID, doctorID)
+// 	if err != nil {
+// 		return nil, err
+// 	}
+
+// 	if consultation.DoctorID != doctorID {
+// 		return nil, errors.New("unauthorized access to consultation")
+// 	}
+
+// 	return consultation, nil
+// }
+
+// // Menambahkan rekomendasi untuk konsultasi
+// func (uc *ConsultationUsecaseImpl) AddRecommendation(doctorID, consultationID int, recommendation string) error {
+// 	consultation, err := uc.Repo.GetConsultationDetails(consultationID, doctorID)
+// 	if err != nil {
+// 		return err
+// 	}
+
+// 	if consultation.DoctorID != doctorID {
+// 		return errors.New("unauthorized access to consultation")
+// 	}
+
+// 	recommendationObj := &model.Rekomendasi{
+// 		ConsultationID: consultation.ID,
+// 		DoctorID:       doctorID,
+// 		Rekomendasi:    recommendation,
+// 	}
+// 	return uc.Repo.AddRecommendation(recommendationObj)
+// }
+
+// // Mendapatkan daftar konsultasi dengan dokter untuk user tertentu
+// func (uc *ConsultationUsecaseImpl) GetUserConsultations(userID int) ([]model.Consultation, error) {
+// 	return uc.Repo.GetConsultationsWithDoctors(userID)
+// }
+
+// // Mendapatkan daftar konsultasi yang belum disetujui pembayarannya
+// func (uc *ConsultationUsecaseImpl) GetPendingConsultationsForAdmin() ([]model.Consultation, error) {
+// 	return uc.Repo.GetPendingConsultations()
+// }
+
+// 	// Generate Snap Token
+// 	snapTokenResp, err := client.CreateTransaction(snapReq)
+// 	if err != nil {
+// 		return "", fmt.Errorf("failed to create midtrans payment: %w", err)
+// 	}
+
+// 	return snapTokenResp.RedirectURL, nil
+// }
+
+// func (uc *ConsultationUsecaseImpl) VerifyPayment(consultationID int) (string, error) {
+// 	client := coreapi.Client{}
+// 	client.New(os.Getenv("MIDTRANS_SERVER_KEY"), midtrans.Sandbox) // Gunakan midtrans.Production jika live
+
+// 	// Gunakan orderID untuk mencari status transaksi
+// 	orderID := fmt.Sprintf("consultation-%d", consultationID)
+
+// 	// Panggil API untuk mendapatkan status transaksi
+// 	transactionStatusResp, err := client.CheckTransaction(orderID)
+// 	if err != nil {
+// 		return "", fmt.Errorf("failed to verify payment: %w", err)
+// 	}
+
+// 	return transactionStatusResp.TransactionStatus, nil
+// }
