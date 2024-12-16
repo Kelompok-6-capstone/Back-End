79a8bcc93191744be833ee34f37c10f202b5a08e
mono <monoberutu58@gmail.com>
2024-12-16 20:45:57 +0700

    workflow rename

--- controller/statistik/stats_controller.go
+++ controller/statistik/stats_controller.go
@@ -12,21 +12,32 @@ type StatsControllerImpl struct {
 	Usecase usecase.StatsUsecase
 }
 
+// DTO untuk statistik
+type StatsResponse struct {
+	TotalUsers         int64 `json:"totalUsers"`
+	TotalDoctors       int64 `json:"totalDoctors"`
+	TotalConsultations int64 `json:"totalConsultations"`
+	TotalPaid          int64 `json:"totalPaid"`
+	TotalPending       int64 `json:"totalPending"`
+}
+
 func NewStatsController(usecase usecase.StatsUsecase) *StatsControllerImpl {
 	return &StatsControllerImpl{Usecase: usecase}
 }
 
 func (sc *StatsControllerImpl) GetStats(ctx echo.Context) error {
-	totalUsers, totalDoctors, totalPaid, totalPending, totalConsultations, err := sc.Usecase.GetStats()
+	totalUsers, totalDoctors, totalConsultations, totalPaid, totalPending, err := sc.Usecase.GetStats()
 	if err != nil {
 		return helper.JSONErrorResponse(ctx, http.StatusInternalServerError, "Failed to fetch statistics: "+err.Error())
 	}
 
-	return helper.JSONSuccessResponse(ctx, map[string]any{
-		"totalUsers":         totalUsers,
-		"totalDoctors":       totalDoctors,
-		"totalPaid":          totalPaid,
-		"totalPending":       totalPending,
-		"totalConsultations": totalConsultations,
-	})
+	stats := StatsResponse{
+		TotalUsers:         totalUsers,
+		TotalDoctors:       totalDoctors,
+		TotalConsultations: totalConsultations,
+		TotalPaid:          totalPaid,
+		TotalPending:       totalPending,
+	}
+
+	return helper.JSONSuccessResponse(ctx, stats)
 }
