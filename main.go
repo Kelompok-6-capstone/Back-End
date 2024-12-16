13fb6b3e78dfbf93c2e8db5c85538c8b5f5fcea6
mono <monoberutu58@gmail.com>
2024-12-16 14:38:36 +0700

    capstone project fix

--- main.go
+++ main.go
@@ -140,7 +140,6 @@ func main() {
 		return c.NoContent(http.StatusOK)
 	})
 
-	http.HandleFunc("/ws", helper.HandleConnections)
 	// routes auth
 	routes.UserAuthRoutes(e, userController)               // user
 	routes.AdminAuthRoutes(e, adminController)             // admin
